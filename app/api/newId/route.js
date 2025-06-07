91b8b95272dfac9f2860e85c4a67dcc4b40c0ad3
kaushal \saraf <kaushalsaraf2002@gmail.com>
2025-06-07 09:43:42 +0530

    first api created

--- app/api/newId/route.js
+++ app/api/newId/route.js
@@ -1,11 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    const newId = uuidv4();
-    res.status(200).json({ id: newId });
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+export async function GET(request) {
+  const newId = uuidv4();
+  return Response.json({ id: newId });
 }
