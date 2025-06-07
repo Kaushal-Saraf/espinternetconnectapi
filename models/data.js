11b65140f50c2c159ca10ddbdd1efca3f62ce325
kaushal \saraf <kaushalsaraf2002@gmail.com>
2025-06-07 15:47:10 +0530

    hello

--- models/data.js
+++ models/data.js
@@ -1,9 +1,12 @@
 const { Schema, default: mongoose } = require("mongoose");
 
 const dataSchema=new Schema({
-    uid:{
+    userData:{
         type:JSON,
     },
+    espData:{
+        type:JSON,
+    }
 })
 export const data=
  mongoose.models.data || mongoose.model("data",dataSchema);
