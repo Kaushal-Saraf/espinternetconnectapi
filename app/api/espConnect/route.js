7b42b689d963347b65fc2692083ab227f3efa85c
kaushal \saraf <kaushalsaraf2002@gmail.com>
2025-06-09 11:44:27 +0530

    hello

--- app/api/espConnect/route.js
+++ app/api/espConnect/route.js
@@ -1,75 +1,24 @@
 import { connectDb } from "@/helper/db";
-import { data } from "@/models/data";
+import { data, data } from "@/models/data";
 
-export async function POST(req, { params }) {
+export async function POST(req) {
 
-  await connectDb();
-//data
-//   const machinedata = await machine.findOne({ umid: params.machineid });
+    await connectDb();
+    const { id } = await req.json();
 
-//   const prescriptionDetails = await prescription.findOne({ _id: params.id });
-//   if (prescriptionDetails.status) {
-//     return NextResponse.json(
-//       { message: "Medicines alreday bought" },
-//       { status: 403 }
-//     );
-//   }
+    if (!id) {
+      return NextResponse.json({ error: 'ID is required' }, { status: 400 });
+    }
 
-//   const compare = prescriptionDetails.medicines.every((med1) =>
-//     machinedata.medicinedetails.some(
-//       (med2) =>
-//         med2.name === med1.name &&
-//         med2.cpsuleeachpack * med2.notsold >=
-//           med1.timeperiod * med1.dailyfrequency
-//     )
-//   );
+    const espData = await data.findOne({ id: id });
 
-//   if (!compare) {
-//     return NextResponse.json(
-//       { message: "Medicne not available in the machine" },
-//       { status: 403 }
-//     );
-//   }
+    if (!espData) {
+      return Response.json({ message: 'ID not found' }, { status: 404 });
+    }
 
-//   // only for unique elements in array
-//   const reqarray = prescriptionDetails.medicines.map((medicine) => {
-//     const matchedMedicine = machinedata.medicinedetails.find(
-//       (machinemedicine) => {
-//         return machinemedicine.name === medicine.name;
-//       }
-//     );
-//     return Number(matchedMedicine.slot);
-//   });
-//   reqarray.forEach((slot) => {
-//     const matchedMedicine = machinedata.medicinedetails.find(
-//       (medicine) => medicine.slot == slot
-//     );
-//     if (matchedMedicine) {
-//       matchedMedicine.notsold--;
-//       matchedMedicine.soldandnotcollected++;
-//     }
-//   });
-//   reqarray.sort((a, b) => a - b);
-//   const qrarray = reqarray.map((value) => [value, 1]).flat();
-//   const d = new Date();
-//   const newqr = {
-//     uid: String(prescriptionDetails.aadharnumber) + String(d.getTime()),
-//     aadhar: prescriptionDetails.aadharnumber,
-//     umid: params.machineid,
-//     medicinedata: qrarray,
-//     used: false,
-//   };
-
-//   await prescription.updateOne({ _id: params.id }, { status: true });
-
-//   await qr.create(newqr);
-
-//   await machine.updateOne(
-//     { umid: params.machineid },
-//     { medicinedetails: machinedata.medicinedetails }
-//   );
-//   const patientid = await patient.findOne({
-//     aadharnumber: prescriptionDetails.aadharnumber,
-//   });
-  return Response.json({message: "Hello"});
+    if (espData.userData) {
+      return Response.json({ userData: espData.userData }, { status: 200 });
+    } else {
+      return Response.json({ message: 'no data' }, { status: 200 });
+    }
 }
--- app/api/newId/route.js
+++ app/api/newId/route.js
@@ -1,6 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
-
+import { connectDb } from "@/helper/db";
+import { data } from "@/models/data";
+import { v4 as uuidv4 } from "uuid";
 export async function GET(request) {
   const newId = uuidv4();
+  await connectDb();
+  await data.create({ id: newId });
   return Response.json({ id: newId });
 }
